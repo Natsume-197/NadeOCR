d0f638bccf3ddb895c341af193915995199b2e7a
Jonathan <36428207+Natsume-197@users.noreply.github.com>
2021-12-26 19:45:29 -0500

    Add support to PyInstaller and texthooker option is now working

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from tkinter.constants import FALSE
 import cv2
 import sys
 import io
@@ -14,15 +15,32 @@ from google.oauth2 import service_account
 from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu
 import toast
+from time import sleep
+from PyQt5.QtCore import Qt
+import configparser
 
 class App:
     def __init__(self):
         self.app = QApplication(sys.argv)
+
+        # Initil value obtained from config file for isSplit   
+
         icon = QIcon('./Resources/icon.png')
         menu = QMenu()
 
         self.scanAction = menu.addAction('Escanear')
         self.scanAction.triggered.connect(self.crop)
+
+        self.splitAction = menu.addAction('Modo Texthooker')
+        self.splitAction.setCheckable(True)
+        self.splitAction.triggered.connect(self.split)
+        self.splitAction.setChecked(to_bool(dict_config["isSplit"]))
+        
+        self.mangaAction = menu.addAction('Modo manga')
+        self.mangaAction.setCheckable(True)
+        self.mangaAction.triggered.connect(self.manga)
+        self.mangaAction.setChecked(to_bool(dict_config["isManga"]))
+
         exitAction = menu.addAction('Salir')
         exitAction.triggered.connect(sys.exit)
 
@@ -43,6 +61,12 @@ class App:
         self.app.exec_()
         sys.exit()
 
+    def split(self):
+        editConfig('user_settings', 'isSplit', str(self.splitAction.isChecked()))
+
+    def manga(self):
+        editConfig('user_settings', 'isManga', str(self.mangaAction.isChecked()))
+    
     def crop(self):
         self.window = MyWidget()
         self.window.show()
@@ -51,6 +75,8 @@ class App:
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+        self.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint | Qt.SubWindow )
+        self.setWindowState(Qt.WindowMaximized | Qt.WindowFullScreen)
         root = tk.Tk()
         screen_width = root.winfo_screenwidth()
         screen_height = root.winfo_screenheight()
@@ -58,18 +84,22 @@ class MyWidget(QtWidgets.QWidget):
         self.setWindowTitle(' ')
         self.begin = QtCore.QPoint()
         self.end = QtCore.QPoint()
+        self.setStyleSheet('''
+            MyWidget { 
+                background: black;
+            }
+        ''')
         self.setWindowOpacity(0.3)
         QtWidgets.QApplication.setOverrideCursor(
             QtGui.QCursor(QtCore.Qt.CrossCursor)
         )
-        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         self.w = toast.W()
         self.show()
 
     def paintEvent(self, event):
         qp = QtGui.QPainter(self)
         qp.setPen(QtGui.QPen(QtGui.QColor('black'), 3))
-        qp.setBrush(QtGui.QColor(128, 128, 255, 128))
+        qp.setBrush(QtGui.QColor(255, 255, 255, 128))
         qp.drawRect(QtCore.QRect(self.begin, self.end))
 
     def mousePressEvent(self, event):
@@ -97,16 +127,26 @@ class MyWidget(QtWidgets.QWidget):
 
         response = client.text_detection(image=image)
         texts = response.text_annotations
+        sentences = []
+            
+        dict_config2 = readConfig()
+        isSplit = to_bool(dict_config2["isSplit"])
+
         try:
-            print(texts[0].description)
-            pc.copy(texts[0].description)
+            sentences = texts[0].description.splitlines()
+            if(isSplit == True):
+                for i in range(len(sentences)):
+                    sleep(0.3)
+                    pc.copy(sentences[i])
+            else:
+                pc.copy(texts[0].description)
+            
             wave_obj = sa.WaveObject.from_wave_file(dir_path+'/Resources/sfx-success1.wav')
             play_obj = wave_obj.play()
             self.w.showToaster()
 
         except Exception as error:
             print(error)
-            print("No se encontro texto")
         if response.error.message:
             raise Exception(
                 '{}\nFor more info on error messages, check: '
@@ -139,6 +179,43 @@ class MyWidget(QtWidgets.QWidget):
         except:
             print('Recorte no válido.')
 
+def to_bool(value):
+    if str(value).lower() in ("yes", "y", "true",  "t", "1"): return True
+    if str(value).lower() in ("no",  "n", "false", "f", "0", "0.0", "", "none", "[]", "{}"): return False
+    raise Exception('Invalid value for boolean conversion: ' + str(value))
+
+def readConfig():
+    parser = configparser.ConfigParser()
+    abspath = os.path.abspath(sys.argv[0])
+    dname = os.path.dirname(abspath)
+    path_config = dname+'/config.ini'
+    parser.read(path_config)
+    isSplit = parser.get('user_settings', 'isSplit')
+    isManga = parser.get('user_settings', 'isManga')
+    dict_config = {
+        "isSplit": isSplit,
+        "isManga": isManga
+    }
+    return dict_config
+
+def editConfig(section, option, value):
+    parser = configparser.ConfigParser()
+    abspath = os.path.abspath(sys.argv[0])
+    dname = os.path.dirname(abspath)
+    path_config = dname+'/config.ini'
+    parser.read(path_config)
+
+    with open(path_config, 'w') as configfile:
+        parser.set(section, option, value)
+        parser.write(configfile)
+
+dict_config = readConfig()
+
 if __name__ == "__main__":
-  app = App()
-  app.run()
+    try:
+        app = App()
+        app.run()
+    except Exception as e:
+        file1 = open("log.txt","w")
+        file1.write(str(e))
+        file1.close()
