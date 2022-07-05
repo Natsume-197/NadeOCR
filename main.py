06caa4bccfc97c2161516a053171a4febaa66d69
David Afonso <davafons@gmail.com>
2022-07-05 23:15:54 +0200

    Solve incompatibilities with Linux

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
+import platform
 import sys
-import ctypes
 from GUI.tray_widget import SystemTray
 from PySide6.QtWidgets import QApplication 
 
 class MainApp: 
     def __init__(self):
-        # Assigning AppUserModelID in order to fix taskbar's icons for Windows
-        id_app = 'natsume.nadeocr.0.3.0' 
-        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(id_app)
+        # OS specific setup during application startup
+        self.__os_specific_setup()
         # Keep the app running after the last window closed
         QApplication.setQuitOnLastWindowClosed(False) 
         # Main instance of NadeOCR
@@ -18,6 +17,14 @@ class MainApp:
     # Keeps the main process running until exit
     def run(self):
         sys.exit(self._app.exec())
+
+    def __os_specific_setup(self):
+        if platform.system() == "Windows":
+            import ctypes
+            # Assigning AppUserModelID in order to fix taskbar's icons for Windows
+            id_app = 'natsume.nadeocr.0.3.0' 
+            ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(id_app)
+
         
 if __name__ == '__main__':
     app = MainApp()
