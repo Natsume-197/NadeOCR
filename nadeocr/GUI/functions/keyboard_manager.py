059597c511d3e0dd84a959a57895b8b3ee02acae
David Afonso <davafons@gmail.com>
2022-09-16 21:56:22 +0200

    Replace keyboards dependency with pynput to fix root access required issue on Linux

--- nadeocr/GUI/functions/keyboard_manager.py
+++ nadeocr/GUI/functions/keyboard_manager.py
@@ -1,12 +1,34 @@
-import keyboard
 from PySide6.QtCore import Signal, QObject
 from nadeocr.GUI.functions.utils.extra import read_config_ini
+from pynput import keyboard
 
 # Listener for keyboard events using PySide6 and Signal
 class KeyBoardManager(QObject):
     F1Signal = Signal()
+
     def start(self):
         config_reader = read_config_ini()
         shortcut_key = config_reader["user_settings"]["shortcut_key"]
-        keyboard.add_hotkey(shortcut_key, self.F1Signal.emit)
-        
+
+        capture_hotkey = keyboard.HotKey(
+            keyboard.HotKey.parse(shortcut_key), self.F1Signal.emit
+        )
+
+        # TODO: Ideally this should be defined on __init__ to avoid having to check if listener is defined.
+        if hasattr(self, "listener"):
+            print("Resetting keyboard listener thread")
+            self.listener.stop()
+
+        self.listener = keyboard.Listener(
+            on_press=self.for_canonical(capture_hotkey.press),
+            on_release=self.for_canonical(capture_hotkey.release),
+        )
+
+        self.listener.start()
+
+    def for_canonical(self, f):
+        # canonical() removes any modifiers and returns the actual key pressed.
+        # Examples:
+        #  * Shift+a returns lowercase a, instead of uppercase A
+        #  * Ctrl+<alphanumeric key> on japanese keyboards return the pressed key, instead of a composed character.
+        return lambda k: f(self.listener.canonical(k))
